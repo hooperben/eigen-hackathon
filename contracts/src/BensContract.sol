7bdd3153250db8d5298b56e5a8091167a6dc0887
Ben Hooper <ben@labrys.io>
2024-06-08 11:10:40 +0200

    feat: added placeholder + fixed test breaking

--- contracts/src/BensContract.sol
+++ contracts/src/BensContract.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.13;
 contract BensContract {
     event BensEvent(uint256 indexed number);
 
+    event BensEvent2(uint256 indexed number);
+
     function setNumber(uint256 newNumber) public {
         emit BensEvent(newNumber);
     }
