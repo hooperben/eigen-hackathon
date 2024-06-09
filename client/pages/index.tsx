741cfc4907bd11a084b313db2bbe0a3ac50c95c9
Ben Hooper <ben@labrys.io>
2024-06-09 16:01:53 +0200

    chore: added some more text to the front end

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -26,12 +26,12 @@ const Home: NextPage = () => {
   const { address } = useAccount();
 
   const [sourceNetwork, setSourceNetwork] = useState<string>("holesky");
-  const [destNetwork, setDestNetwork] = useState<string>("optimism-sepolia");
+  const [destNetwork, setDestNetwork] = useState<string>("OP Sepolia");
 
   const [sourceToken, setSourceToken] = useState<string>("ETH");
   const [destToken, setDestToken] = useState<string>("USDC");
 
-  const supportedNetworks = ["holesky", "optimism-sepolia"];
+  const supportedNetworks = ["holesky", "OP Sepolia"];
   const supportedTokens = ["ETH", "USDC"];
 
   return (
@@ -56,85 +56,98 @@ const Home: NextPage = () => {
 
               <CardContent>
                 {/* SOURCE CHAIN AND TOKEN */}
-                <div className="p-4 flex justify-between">
-                  <DropdownMenu>
-                    <DropdownMenuTrigger asChild className="w-40">
-                      <Button variant="outline">{sourceNetwork}</Button>
-                    </DropdownMenuTrigger>
+                <div>
+                  <div className="pl-4 flex align-left text-xs pb-1">
+                    <p>Source Network and Token</p>
+                  </div>
+                  <div className="px-4 flex justify-between">
+                    <DropdownMenu>
+                      <DropdownMenuTrigger asChild className="w-40">
+                        <Button variant="outline">{sourceNetwork}</Button>
+                      </DropdownMenuTrigger>
 
-                    <DropdownMenuContent className="w-40">
-                      {supportedNetworks.map((network) => (
-                        <DropdownMenuCheckboxItem
-                          key={network}
-                          checked={false}
-                          onCheckedChange={() => setSourceNetwork(network)}
-                        >
-                          {network}
-                        </DropdownMenuCheckboxItem>
-                      ))}
-                    </DropdownMenuContent>
-                  </DropdownMenu>
+                      <DropdownMenuContent className="w-40">
+                        {supportedNetworks.map((network) => (
+                          <DropdownMenuCheckboxItem
+                            key={network}
+                            checked={false}
+                            onCheckedChange={() => setSourceNetwork(network)}
+                          >
+                            {network}
+                          </DropdownMenuCheckboxItem>
+                        ))}
+                      </DropdownMenuContent>
+                    </DropdownMenu>
 
-                  <DropdownMenu>
-                    <DropdownMenuTrigger asChild className="w-60">
-                      <Button variant="outline">{sourceToken}</Button>
-                    </DropdownMenuTrigger>
+                    <DropdownMenu>
+                      <DropdownMenuTrigger asChild className="w-60">
+                        <Button variant="outline">{sourceToken}</Button>
+                      </DropdownMenuTrigger>
 
-                    <DropdownMenuContent className="w-60">
-                      {supportedTokens.map((token) => (
-                        <DropdownMenuCheckboxItem
-                          key={token}
-                          checked={token === sourceToken}
-                          onCheckedChange={() => setSourceToken(token)}
-                        >
-                          {token}
-                        </DropdownMenuCheckboxItem>
-                      ))}
-                    </DropdownMenuContent>
-                  </DropdownMenu>
+                      <DropdownMenuContent className="w-60">
+                        {supportedTokens.map((token) => (
+                          <DropdownMenuCheckboxItem
+                            key={token}
+                            checked={token === sourceToken}
+                            onCheckedChange={() => setSourceToken(token)}
+                          >
+                            {token}
+                          </DropdownMenuCheckboxItem>
+                        ))}
+                      </DropdownMenuContent>
+                    </DropdownMenu>
+                  </div>
                 </div>
 
                 {/* DEST CHAIN AND TOKEN */}
-                <div className="p-4 flex justify-between">
-                  <DropdownMenu>
-                    <DropdownMenuTrigger asChild className="w-40">
-                      <Button variant="outline">{destNetwork}</Button>
-                    </DropdownMenuTrigger>
+                <div className="pt-5">
+                  <div className="pl-4 flex align-left text-xs pb-1">
+                    <p>Destination Network and Token</p>
+                  </div>
+                  <div className="px-4 flex justify-between">
+                    <DropdownMenu>
+                      <DropdownMenuTrigger asChild className="w-40">
+                        <Button variant="outline">{destNetwork}</Button>
+                      </DropdownMenuTrigger>
 
-                    <DropdownMenuContent className="w-40">
-                      {supportedNetworks.map((network) => (
-                        <DropdownMenuCheckboxItem
-                          key={network}
-                          checked={destNetwork === network}
-                          onCheckedChange={() => setDestNetwork(network)}
-                        >
-                          {network}
-                        </DropdownMenuCheckboxItem>
-                      ))}
-                    </DropdownMenuContent>
-                  </DropdownMenu>
+                      <DropdownMenuContent className="w-40">
+                        {supportedNetworks.map((network) => (
+                          <DropdownMenuCheckboxItem
+                            key={network}
+                            checked={destNetwork === network}
+                            onCheckedChange={() => setDestNetwork(network)}
+                          >
+                            {network}
+                          </DropdownMenuCheckboxItem>
+                        ))}
+                      </DropdownMenuContent>
+                    </DropdownMenu>
 
-                  <DropdownMenu>
-                    <DropdownMenuTrigger asChild className="w-60">
-                      <Button variant="outline">{destToken}</Button>
-                    </DropdownMenuTrigger>
+                    <DropdownMenu>
+                      <DropdownMenuTrigger asChild className="w-60">
+                        <Button variant="outline">{destToken}</Button>
+                      </DropdownMenuTrigger>
 
-                    <DropdownMenuContent className="w-60">
-                      {supportedTokens.map((token) => (
-                        <DropdownMenuCheckboxItem
-                          key={token}
-                          checked={destToken === token}
-                          onCheckedChange={() => setDestToken(token)}
-                        >
-                          {token}
-                        </DropdownMenuCheckboxItem>
-                      ))}
-                    </DropdownMenuContent>
-                  </DropdownMenu>
+                      <DropdownMenuContent className="w-60">
+                        {supportedTokens.map((token) => (
+                          <DropdownMenuCheckboxItem
+                            key={token}
+                            checked={destToken === token}
+                            onCheckedChange={() => setDestToken(token)}
+                          >
+                            {token}
+                          </DropdownMenuCheckboxItem>
+                        ))}
+                      </DropdownMenuContent>
+                    </DropdownMenu>
+                  </div>
                 </div>
 
                 {/* AMOUNT */}
                 <div className="p-4">
+                  <div className="flex align-left text-xs pb-1">
+                    <p>Amount to Send</p>
+                  </div>
                   <Input
                     className="bg-white"
                     type="number"
